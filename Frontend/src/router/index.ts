c82b53642c0ff7ae7c8745535bf29e38b98dafa9
Jonablo <jonasanchez1993@gmail.com>
2025-11-19 13:04:08 +0000

    Chore: Configure integration for Cloudflare worker functionality

--- Frontend/src/router/index.ts
+++ Frontend/src/router/index.ts
@@ -1,12 +1,26 @@
-import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
-import UserView from '../views/UserView.vue'
+import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router';
+import UserView from '../views/UserView.vue';
 
 const routes: RouteRecordRaw[] = [
   { path: '/', redirect: '/user' },
   { path: '/user', component: UserView }
 ]
 
-export default createRouter({
+const router = createRouter({
   history: createWebHistory(),
-  routes
-})
+  routes,
+});
+
+//marcador
+router.afterEach((to, from) => {
+  fetch("https://gentle-breeze-d28f.jonasanchez1993.workers.dev/track", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      tipo: "frontend",
+      ruta: "http://localhost:5173"
+    })
+  });
+});
+
+export default router;
--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -3,5 +3,9 @@ import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
 
 export default defineConfig({
+  server: {
+    host: true,
+    port: 5173,
+  },
   plugins: [vue(), vuetify({ autoImport: true })]
 })
