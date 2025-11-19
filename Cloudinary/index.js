741b0f0fd7f7ae04c699a77009fe9e6f6691c10b
AlexandraN2001 <nacabrera@uce.edu.ec>
2025-11-19 11:55:40 -0500

    Modificando index.js agg .env

--- Cloudinary/index.js
+++ Cloudinary/index.js
@@ -1,23 +1,61 @@
+require("dotenv").config();
+const express = require("express");
+const multer = require("multer");
+const axios = require("axios");
+const cloudinary = require("cloudinary").v2;
+const cors = require("cors");
+const app = express();
+app.use(cors());
+
+const upload = multer({ storage: multer.memoryStorage() });
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 app.post("/upload", upload.single("photo"), async (req, res) => {
-  const file = req.file;
+   console.log("Petición recibida en /upload");
+   const file = req.file;
+    if (!file) return res.status(400).json({ error: "No file provided" });
 
-  const cloud_name = "drsxop82w";
-  const upload_preset = "visits_preset";
+    try {
+      const folder = "visits";
+      const timestamp = Math.round(Date.now() / 1000);
 
-  const base64Image =
-    `data:${file.mimetype};base64,${file.buffer.toString("base64")}`;
+      // Generar firma segura
+      const signature = cloudinary.utils.api_sign_request(
+        { folder, timestamp },
+        process.env.CLOUDINARY_API_SECRET
+      );
+
+  const fileBase64 =`data:${file.mimetype};base64,${file.buffer.toString("base64")}`;
 
   const response = await axios.post(
-    `https://api.cloudinary.com/v1_1/${cloud_name}/image/upload`,
+    `https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/image/upload`,
     {
-      file: base64Image,
-      upload_preset,
-      folder: "visits"
+      file: fileBase64,
+        folder,
+        api_key: process.env.CLOUDINARY_API_KEY,
+        timestamp,
+        signature,
     }
   );
 
-  res.json({
-    ok: true,
-    cloudinary: response.data,
+  return res.json({
+      ok: true,
+      cloudinary: response.data,
+    });
+} catch (error) {
+    console.log("ERROR SUBIENDO A CLOUDINARY");
+    console.log(error.response?.data || error);
+    console.log("----------------------------");
+
+  return res.status(500).json({
+    error: "Upload failed",
+    details: error.response?.data || error.message
   });
+  }
 });
+app.listen(3000, () => console.log("Servidor listo en http://localhost:3000"));
